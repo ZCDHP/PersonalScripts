8b023289df5f8f6d2048accc8e6703a9986d89e4
ZCDHP <feedem_F@163.com>
2019-06-22 14:04:32 +0000

    Add docker compose

--- debian/dockor.sh
+++ debian/dockor.sh
@@ -5,3 +5,6 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
 yes | apt-get install docker-ce docker-ce-cli containerd.io
+# docker compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
